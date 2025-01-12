4252833d58c8c9f89166501691c211850734cc57
Dolev Harel <96368150+Dolevscom@users.noreply.github.com>
2025-01-12 11:41:08 +0200

    bargrpah and ampereguage

---
+++ hydrogen-rocket-ui/src/components/AmpereGuage/AmpereGauge.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./AmpereGauge.css";
+import container from "/Users/dolevsmac/PycharmProjects/Hydrogen_Rocket_react/hydrogen-rocket-ui/src/assets/Components/Gauge/container.png";
+import pointer from "/Users/dolevsmac/PycharmProjects/Hydrogen_Rocket_react/hydrogen-rocket-ui/src/assets/Components/Gauge/pointer_S.png";
+
+const AmpereGauge = ({ currentValue, maxValue }) => {
+  const needleAngle = (currentValue / maxValue) * 180;
+
+  return (
+    <div className="gauge-container">
+      {/* Background image of the gauge */}
+      <img
+        src={container}
+        alt="Gauge Background"
+        className="gauge-image"
+      />
+      {/* Needle */}
+      <div
+        className="gauge-needle"
+        style={{
+          transform: `rotate(${needleAngle - 90}deg)`, // Rotate needle
+          backgroundImage: `url(${pointer})`, // Use pointer image
+        }}
+      ></div>
+      {/* Display Current Value */}
+      {/*<div className="gauge-value">*/}
+      {/*  <p>{`${currentValue} A`}</p>*/}
+      {/*</div>*/}
+    </div>
+  );
+};
+
+export default AmpereGauge;
